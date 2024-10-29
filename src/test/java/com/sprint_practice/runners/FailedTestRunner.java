f69555158595b4b5d83938d011409eb4ec41c5b0
Zumret910 <tzumuret@gmail.com>
2024-10-29 09:54:55 -0400

    Initial commit

---
+++ src/test/java/com/sprint_practice/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.sprint_practice.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/sprint_practice/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
